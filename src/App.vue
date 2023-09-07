20e3232cac974dae564ac21f1c40704ab93a0de6
Kacper <kukulakacper@outlook.com>
2023-09-07 13:25:44 +0200

    init

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,37 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <nav>
+    <router-link to="/numeric-methods" class="nav-cell">Metody numeryczne</router-link>
+    <router-link to="/" class="nav-cell"><img class="logo" src="./assets/LOGO.png" alt="GOMATH"></router-link>
+    <router-link to="/numeric-methods" class="nav-cell">Metody numeryczne</router-link>
+  </nav>
+  <router-view/>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+<style scoped>
+nav {
+  padding: 30px;
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+  gap: 4vw;
 }
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+nav .nav-cell {
+  max-width: 12vw;
+  font-weight: 500;
+  overflow-x: hidden;
+  font-size: 1.1rem;
+  text-decoration: none;
+  color: var(--secondary-color);
 }
+
+nav .nav-cell.router-link-exact-active {
+  color: var(--primary-color);
+}
+
+/*main logo */
+.logo {
+ width: clamp(120px, 100%, 320px);
+}
+
 </style>
